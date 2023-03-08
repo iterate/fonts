f2c5bac102fcc9692bec526c0f551e73bc125eac
Thusan Arul <thusan.arul@iterate.no>
2023-03-08 19:23:50 +0100

    Starts on headless chrome browser crawler using headless_chrome crate

--- src/crawler/browser_crawler.rs
+++ src/crawler/browser_crawler.rs
@@ -1,1 +1,14 @@
+use headless_chrome::Browser;
 
+use eyre::{eyre, Result};
+
+pub struct BrowserCrawler {
+    client: Browser,
+}
+
+impl BrowserCrawler {
+    pub fn new() -> Result<Self> {
+        let client = Browser::default().map_err(|err| eyre!(err))?;
+        Ok(BrowserCrawler { client })
+    }
+}

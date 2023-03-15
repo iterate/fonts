634f4bc13d0cb5c18e953d8eab75f897eb1d9f5e
Thusan Arul <thusan.arul@iterate.no>
2023-03-15 21:09:58 +0100

    Adds inline text_css elements to html parser function, and updates tests

--- src/crawler/browser_crawler.rs
+++ src/crawler/browser_crawler.rs
@@ -36,25 +36,11 @@ mod tests {
     #[test]
     #[cfg_attr(not(feature = "network"), ignore)]
     fn get_elements_with_browser_crawler() -> Result<()> {
+        // This test is mainly used for developing.
+        // But, should probably find (or, create) a stable site that outputs something I could use for this test
         let browser = BrowserCrawler::new()?;
 
-        let content = browser.get_page_content("https://iterate.no")?;
-
-        let elements = get_elements_from_page(&content);
-        let expected_results = vec![Element::CssLink("https://uploads-ssl.webflow.com/5ea18b09bf3bfd55814199f9/css/iterate-104ab8-23d141065ef1b8634c6a653a.webflow.f3ca629db.css".to_owned())];
-
-        assert_eq!(elements, expected_results);
-
         let content = browser.get_page_content("https://ense.no")?;
-
-        let elements = get_elements_from_page(&content);
-
-        // is not a font link. need to implement inline-css thing to actually get font from ense.no
-        let expected_results = vec![Element::CssLink(
-            "main.5606dde6c1acfbce1170bda109e0b739.css".to_owned(),
-        )];
-
-        assert_eq!(elements, expected_results);
 
         Ok(())
     }

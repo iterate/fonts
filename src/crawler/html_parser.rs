1f0e8b2478c64eaa28d2b788826594912592f1b0
Thusan Arul <thusan.arul@iterate.no>
2023-03-15 18:24:31 +0100

    renames variable to element

--- src/crawler/html_parser.rs
+++ src/crawler/html_parser.rs
@@ -1,6 +1,5 @@
 use std::collections::HashSet;
 
-use eyre::{eyre, Result};
 use maplit::hashset;
 use once_cell::sync::Lazy;
 use scraper::Html;
@@ -91,15 +90,15 @@ mod tests {
         let html_file =
             fs::read_to_string("test_files/test_iterateno.html").expect("Could not load html file");
 
-        let links = get_elements_from_page(&html_file);
+        let elements = get_elements_from_page(&html_file);
         let expected_results = vec![Element::CssLink("https://uploads-ssl.webflow.com/5ea18b09bf3bfd55814199f9/css/iterate-104ab8-23d141065ef1b8634c6a653a.webflow.f3ca629db.css".to_owned())];
 
-        assert_eq!(links, expected_results);
+        assert_eq!(elements, expected_results);
 
         let html_file =
             fs::read_to_string("test_files/test_nrkno.html").expect("Could not load html file");
 
-        let links = get_elements_from_page(&html_file);
+        let elements = get_elements_from_page(&html_file);
         let expected_results = vec![Element::FontLink("https://static.nrk.no/nrk-sans/1.2.1/NRKSans_Variable.woff2".to_owned()), 
                                     Element::CssLink("https://static.nrk.no/publisering/kurator-visning/assets/index-4167d179.css".to_owned()), 
                                     Element::CssLink("https://static.nrk.no/dh/module/nrkno-eksperimenter/assets/front-module.5c672c95.css".to_owned()), 
@@ -107,7 +106,7 @@ mod tests {
                                     Element::CssLink("https://static.nrk.no/dh/module/langlesing/static//langlesingEtasje-33739c845c1bc1af2a00.css".to_owned()), 
                                     Element::CssLink("https://static.nrk.no/nrkno/serum/2.0.484/singelton/bottommenu/bottommenu.css".to_owned())];
 
-        assert_eq!(links, expected_results);
+        assert_eq!(elements, expected_results);
 
         Ok(())
     }
